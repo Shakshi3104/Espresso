21a2b4b1f8d304c3dbe1f8aa6a8db1571a6dabb6
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2021-11-17 10:53:37 +0900

    Tested

--- Tests/EspressoTests/EspressoTests.swift
+++ Tests/EspressoTests/EspressoTests.swift
@@ -1,11 +1,44 @@
 import XCTest
 @testable import Espresso
+import AVFoundation
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import Cocoa
+#endif
 
 final class EspressoTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Espresso().text, "Hello, World!")
+    func testEspressoInitAVDepthDataViaCIImage() {
+        let url = Bundle.module.url(forResource: "SoftCream", withExtension: "heic")
+        if let url = url {
+            let depthData = AVDepthData.fromURL(url)
+            XCTAssertNotNil(depthData)
+        }
+    }
+    
+    func testEspressoBrewDepthDataImage() {
+        let url = Bundle.module.url(forResource: "SoftCream", withExtension: "heic")
+        guard let url = url else {
+            return
+        }
+        let depthData = AVDepthData.fromURL(url)
+        XCTAssertNotNil(depthData)
+        
+        guard let depthData = depthData else {
+            return
+        }
+        
+        let processor = DepthDataProcessor(depthData)
+        #if os(iOS)
+        let uiImage = UIImage(named: "SoftCream", in: Bundle.module, compatibleWith: nil)
+        guard let uiImage = uiImage else {
+            return
+        }
+        let depthImage = processor.uiImage(orientation: uiImage.imageOrientation, depthType: .depth)
+        XCTAssertNotNil(depthImage)
+        #elseif os(macOS)
+        let depthImage = processor.nsImage(depthType: .depth)
+        XCTAssertNotNil(depthImage)
+        #endif
     }
 }
